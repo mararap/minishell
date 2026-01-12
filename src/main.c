b8fd5c39815f72b9837da16b1929c743bdb73e24
marapovi <marapovi@student.42vienna.com>
2026-01-12 20:07:50 +0100

    added comment to main.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marapovi <marapovi@student.42vienna.com>   +#+  +:+       +#+        */
+/*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 20:32:50 by marapovi          #+#    #+#             */
-/*   Updated: 2026/01/11 21:35:06 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/01/12 16:49:07 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	g_signal_number = 0;
 static void	ms_detect_interactive(t_shell *shell)
 {
 	if (isatty(STDIN_FILENO))
+		//system function to check if STDIN is "at tty"/connected to a terminal
 		shell->is_interactive = 1;
 	else
 		shell->is_interactive = 0;
@@ -30,4 +31,7 @@ int	main(int ac, char **av, char **envp)
 	(void)av;
 	ms_init_shell(&shell, envp);
 	ms_detect_interactive(&shell);
+	ms_main_loop(&shell);
+	ms_free_shell(&shell);
+	return (shell.last_exit_status);
 }
