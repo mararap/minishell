f26c2adff44144a8ce5a76f8b2aa8b677636ac61
Juliyan Atanasov <jatanaso@c3r6p4.42vienna.com>
2026-02-03 15:08:18 +0100

    fix echo and env and add comments

--- src/builtin/builtins_pwd.c
+++ src/builtin/builtins_pwd.c
@@ -5,6 +5,15 @@ int	ms_builtin_pwd(t_shell *shell)
 	char	*cwd;
 
 	(void)shell;
+	
+	/* getcwd -> Get the pathname of the current working directory,
+   and put it in SIZE bytes of BUF.  Returns NULL if the
+   directory couldn't be determined or SIZE was too small.
+   If successful, returns BUF.  In GNU, if BUF is NULL,
+   an array is allocated with `malloc'; the array is SIZE
+   bytes long, unless SIZE == 0, in which case it is as
+   big as necessary.  */
+
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 	{
