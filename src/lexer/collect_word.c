de59a4c0b2c4e324d3f3d0934f8f55724fb757b8
marapovi <marapovi@student.42vienna.com>
2026-02-09 13:10:27 +0100

    did some more fixing on parsing quotes. current state with mstest:
    					TOTAL TEST COUNT: 986  TESTS PASSED: 658
                  STD_OUT: 49  STD_ERR: 287  EXIT_CODE: 153  CRASHING: 0
                              TOTAL FAILED AND PASSED CASES:
                                          ❌ 489
                                          ✅ 2469

--- src/lexer/collect_word.c
+++ src/lexer/collect_word.c
@@ -6,7 +6,7 @@
 /*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:51:20 by marapovi          #+#    #+#             */
-/*   Updated: 2026/02/08 21:02:30 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/02/09 12:47:52 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ static char	*ms_expand_variable(t_shell *shell, char *str, int *idx)
 	start = *idx + 1;
 	while (str[start] && (ft_isalnum(str[start]) || str[start] == '_'))
 		start++;
+	/* If no valid variable name characters found, treat $ as literal */
+	if (start == *idx + 1)
+	{
+		*idx = *idx + 1;
+		return (ft_strdup("$"));
+	}
 	name = ft_substr(str, *idx + 1, start - (*idx + 1));
 	value = ms_env_get_value(shell->env_list, name);
 	free(name);
@@ -50,25 +56,40 @@ static char	*ms_collect_single_quotes(char *str, int *idx)
 	return (tmp);
 }
 
-//be aware: '"' not yet fully implemented (nested, unclosed...)
 static char	*ms_collect_double_quotes(t_shell *shell, char *str, int *idx)
 {
 	int		start;
+	char	*buf;
 	char	*tmp;
+	char	*old_buf;
 
 	(*idx)++;
-	start = *idx;
-	if (str[*idx] == '$')
-		tmp = ms_expand_variable(shell, str, idx);
-	else
+	buf = ft_strdup("");
+	while (str[*idx] && str[*idx] != '"')
 	{
-		while (str[*idx] && str[*idx] != '"')
-			(*idx)++;
-		tmp = ft_substr(str, start, *idx - start);
+		if (str[*idx] == '$')
+		{
+			tmp = ms_expand_variable(shell, str, idx);
+			old_buf = buf;
+			buf = ft_strjoin(buf, tmp);
+			free(old_buf);
+			free(tmp);
+		}
+		else
+		{
+			start = *idx;
+			while (str[*idx] && str[*idx] != '"' && str[*idx] != '$')
+				(*idx)++;
+			tmp = ft_substr(str, start, *idx - start);
+			old_buf = buf;
+			buf = ft_strjoin(buf, tmp);
+			free(old_buf);
+			free(tmp);
+		}
 	}
 	if (str[*idx] == '"')
 		(*idx)++;
-	return (tmp);
+	return (buf);
 }
 
 static char	*ms_collect_plain_word(char *str, int *idx)
