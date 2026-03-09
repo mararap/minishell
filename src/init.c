b2c5605d8060c588a2e9de763deab61e7e87159b
marapovi <marapovi@student.42vienna.com>
2026-03-09 11:38:01 +0100

    init.c - ms_init_shell - made SHLVL-check conditional - should only increment by one of shell is interactive (that's what it does now)

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 16:10:01 by marapovi          #+#    #+#             */
-/*   Updated: 2026/03/09 10:33:43 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/03/09 11:34:06 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,17 @@ void	ms_init_shell(t_shell *shell, char **envp)
 	char	*new_shlvl;
 	
 	shell->env_list = ms_env_from_environ(envp);
-	shlvl_str = ms_env_get_value(shell->env_list, "SHLVL");
-	if (shlvl_str)
-		shlvl = ft_atoi(shlvl_str) + 1; //incremet SHLVL by one on startup
-	else
-		shlvl = 1;
-	new_shlvl = ft_itoa(shlvl);
-	ms_env_set(&shell->env_list, "SHLVL", new_shlvl, 1);
-	free(new_shlvl);
+	if (isatty(STDIN_FILENO))
+	{
+		shlvl_str = ms_env_get_value(shell->env_list, "SHLVL");
+		if (shlvl_str)
+			shlvl = ft_atoi(shlvl_str) + 1; //incremet SHLVL by one on startup
+		else
+			shlvl = 1;
+		new_shlvl = ft_itoa(shlvl);
+		ms_env_set(&shell->env_list, "SHLVL", new_shlvl, 1);
+		free(new_shlvl);
+	}
 	shell->last_exit_status = 0;
 	shell->is_interactive = 0;
 	rl_signal_event_hook = ms_rl_event_hook;
