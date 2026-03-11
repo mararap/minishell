f3eb8d18d85e0470a688d19d08350ba260f9cfcb
marapovi <marapovi@student.42vienna.com>
2026-03-11 15:33:52 +0100

    changed ms_parse_tokens, extracted parts of it to shorten function. two new functions: ms_parse_one_command and ms_parse_pipe_error

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 20:13:43 by marapovi          #+#    #+#             */
-/*   Updated: 2026/01/25 21:25:30 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/03/11 15:14:54 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,34 @@ static void	ms_command_add_back(t_command **list, t_command *new_cmd)
 	iter->next = new_cmd;
 }
 
+static int	ms_parse_pipe_error(t_command *cmds)
+{
+	ft_putstr_fd(SHELL_NAME ": syntax error near unexpected token `|'\n",
+		STDERR_FILENO);
+	ms_free_command_list(cmds);
+	return (1);
+}
+
+static int	ms_parse_one_command(t_command **cmds, t_token **cursor)
+{
+	t_command	*cmd;
+
+	cmd = ms_command_new();
+	if (ms_fill_command(cmd, cursor) < 0)
+	{
+		ms_free_command_list(cmd);
+		ms_free_command_list(*cmds);
+		return (1);
+	}
+	ms_command_add_back(cmds, cmd);
+	if (*cursor && (*cursor)->type == TOKEN_PIPE)
+		*cursor = (*cursor)->next;
+	return (0);
+}
+
 t_command	*ms_parse_tokens(t_token *token_list)
 {
 	t_command	*cmds;
-	t_command	*cmd;
 	t_token		*cursor;
 
 	cmds = NULL;
@@ -50,20 +74,11 @@ t_command	*ms_parse_tokens(t_token *token_list)
 	{
 		if (cursor->type == TOKEN_PIPE)
 		{
-			ft_putstr_fd(SHELL_NAME ": syntax error near unexpected token `|'\n", STDERR_FILENO);
-			ms_free_command_list(cmds);
-			return (NULL);
+			if (ms_parse_pipe_error(cmds))
+				return (NULL);
 		}
-		cmd = ms_command_new();
-		if (ms_fill_command(cmd, &cursor) < 0)
-		{
-			ms_free_command_list(cmd);
-			ms_free_command_list(cmds);
+		if (ms_parse_one_command(&cmds, &cursor))
 			return (NULL);
-		}
-		ms_command_add_back(&cmds, cmd);
-		if (cursor && cursor->type == TOKEN_PIPE)
-			cursor = cursor->next;
 	}
 	return (cmds);
 }
